a0215c17584d8234f9cd7dcf97fd85d067ff780b
Pedro Veloso <contatopedrohalves@gmail.com>
2024-04-10 15:30:48 -0300

    feat: custom error 404 page.

--- lib/kayuk_web/controllers/page_controller.ex
+++ lib/kayuk_web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule KayukWeb.PageController do
   use KayukWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false, count: "{ count: 0 }")
+    render(conn, :home)
   end
 end
