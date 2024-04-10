2712eecdde83af52c0907f2537236ce6f5db4d83
Pedro Veloso <contatopedrohalves@gmail.com>
2024-04-10 08:46:57 -0300

    feat: session schema.

---
+++ lib/kayuk/account/session.ex
@@ -0,0 +1,21 @@
+defmodule Kayuk.Account.Session do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "sessions" do
+    field :ip, :string
+    field :active?, :boolean, default: false
+    field :user_id, :binary_id
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(session, attrs) do
+    session
+    |> cast(attrs, [:ip, :active?])
+    |> validate_required([:ip, :active?])
+  end
+end
