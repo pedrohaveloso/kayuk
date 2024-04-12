ddb3143180ccd7848dceaedb526b2e12a52512e5
Pedro Veloso <contatopedrohalves@gmail.com>
2024-04-12 18:05:36 -0300

    feat: expand menu bar.

--- lib/kayuk_web/components/core_components.ex
+++ lib/kayuk_web/components/core_components.ex
@@ -11,51 +11,72 @@ defmodule KayukWeb.CoreComponents do
   end
 
   def aside_menu(assigns) do
-    assigns =
-      assign(assigns,
-        menu: %{
-          home: %{name: "Home", icon: "hero-home-solid", href: "/"},
-          notifications: %{name: "Notifications", icon: "hero-bell-solid", href: "/"},
-          you: %{name: "You", icon: "hero-user-circle-solid", href: "/"},
-          chats: %{name: "Chats", icon: "hero-chat-bubble-oval-left-ellipsis-solid", href: "/"},
-          boards: %{name: "Boards", icon: "hero-clipboard-document-list-solid", href: "/"},
-          settings: %{name: "Settings", icon: "hero-adjustments-horizontal-solid", href: "/"}
-        }
-      )
+    menu = %{
+      home: %{name: "Home", icon: "hero-home-solid", href: "/home"},
+      notifications: %{name: "Notifications", icon: "hero-bell-solid", href: "/"},
+      you: %{name: "You", icon: "hero-user-circle-solid", href: "/"},
+      chats: %{name: "Chats", icon: "hero-chat-bubble-oval-left-ellipsis-solid", href: "/"},
+      boards: %{name: "Boards", icon: "hero-clipboard-document-list-solid", href: "/"},
+      settings: %{name: "Settings", icon: "hero-adjustments-horizontal-solid", href: "/"}
+    }
+
+    assigns = assign(assigns, menu: menu)
 
     ~H"""
-    <aside class="w-full h-min sm:h-full sm:w-min fixed bottom-0 sm:start-0 p-2">
-      <nav class="flex h-full transition-all duration-100 justify-center bg-slate-100 rounded-xl">
-        <ul class="flex sm:flex-col w-full h-full gap-2 p-2">
-          <.aside_menu_option option={@menu.home} class="hidden sm:flex" />
+    <aside
+      x-data="{ extend: false }"
+      class="h-full fixed bottom-0 start-0 p-2 transition-all duration-500"
+      x-bind:class="extend ? 'w-60' : 'w-20'"
+    >
+      <nav class="flex h-full justify-center bg-gray-100 rounded-3xl p-2 overflow-scroll no-scrollbar ">
+        <ol class="flex flex-col gap-2 items-center justify-center w-full h-full rounded-3xl">
+          <li class="w-full">
+            <button
+              title="Menu"
+              class="min-h-12 min-w-12 w-min hover:bg-gray-200 transition-all duration-500 rounded-full flex items-center justify-center "
+              @click="extend = !extend"
+              type="button"
+            >
+              <.icon class="h-8 w-8 text-gray-700" name="hero-bars-3" />
+            </button>
+          </li>
 
-          <li class="h-full w-full hidden sm:flex"></li>
-
-          <.aside_menu_option option={@menu.you} />
-          <.aside_menu_option option={@menu.notifications} />
-          <.aside_menu_option option={@menu.chats} />
-          <.aside_menu_option option={@menu.boards} />
-
-          <li class="h-full w-full"></li>
+          <li class="grid place-items-center justify-center h-full">
+            <ul class="flex flex-col gap-2">
+              <.aside_menu_option option={@menu.home} />
+              <.aside_menu_option option={@menu.notifications} />
+              <.aside_menu_option option={@menu.chats} />
+              <.aside_menu_option option={@menu.boards} />
+            </ul>
+          </li>
 
           <.aside_menu_option option={@menu.settings} />
-        </ul>
+        </ol>
       </nav>
     </aside>
     """
   end
 
   attr :option, :map
-  attr :rest, :global
 
   def aside_menu_option(assigns) do
     ~H"""
-    <li class={
-      "min-h-12 min-w-12 bg-slate-200 hover:bg-slate-300 transition-all duration-500 rounded-lg flex items-center justify-center "
-      <> if @rest[:class] != nil, do: @rest[:class], else: ""
-    }>
-      <a title={@option.name} href={@option.href} class="flex w-full items-center justify-center">
-        <.icon class="h-8 w-8 text-slate-700" name={@option.icon} />
+    <li>
+      <a
+        title={@option.name}
+        href={@option.href}
+        class="min-h-12 min-w-12 bg-gray-200 hover:bg-gray-300 transition-all duration-500 rounded-full flex items-center"
+        x-bind:class="extend ? 'w-52 ps-2' : 'w-12'"
+      >
+        <div class="text-center p-2">
+          <.icon class="h-8 w-8 text-gray-700" name={@option.icon} />
+        </div>
+
+        <div>
+          <p x-show="extend" x-transition:enter.duration.1000ms x-transition:leave.duration.100ms>
+            <%= @option.name %>
+          </p>
+        </div>
       </a>
     </li>
     """
